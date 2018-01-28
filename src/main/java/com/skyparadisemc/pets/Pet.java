c46c6cabb831286a193a6596624e3d1253538840
xXMaTTHDXx <imatthd01@gmail.com>
2018-01-28 13:46:52 -0700

    Update locations

--- src/main/java/com/skyparadisemc/pets/Pet.java
+++ src/main/java/com/skyparadisemc/pets/Pet.java
@@ -3,7 +3,10 @@ package com.skyparadisemc.pets;
 import com.google.inject.assistedinject.Assisted;
 import com.skyparadisemc.pets.abilities.Ability;
 import lombok.Data;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import javax.inject.Inject;
 
@@ -19,10 +22,36 @@ public class Pet {
 
     private boolean active = false;
 
+    private Entity entity = null;
+
+    private Location location = null;
+
     public @Inject Pet(@Assisted EntityType type, @Assisted String name, @Assisted Ability ability) {
         this.type = type;
         this.name = name;
         this.ability = ability;
         this.active = true;
+    }
+
+    public Location getLocation() {
+        return entity == null ? null : entity.getLocation();
+    }
+
+    public Entity spawn(Location location) {
+        if (this.active) {
+            return null;
+        }
+
+        Entity ent = location.getWorld().spawn(location, type.getEntityClass());
+
+        if (customName.equalsIgnoreCase("")) {
+
+        }
+        else {
+            ent.setCustomName(customName);
+            ent.setCustomNameVisible(true);
+        }
+        this.entity = ent;
+        return ent;
     }
 }
--- src/main/java/com/skyparadisemc/pets/PetService.java
+++ src/main/java/com/skyparadisemc/pets/PetService.java
@@ -8,8 +8,10 @@ import com.skyparadisemc.pets.data.DataService;
 import lombok.Data;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Data
@@ -22,7 +24,29 @@ public class PetService {
 
     private @Inject PetFactory petFactory;
 
+    private @Inject JavaPlugin plugin;
+
     private HashMap<UUID, Pet> playerPets = Maps.newHashMap();
+
+    public void init() {
+        plugin.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, this::updatePets, 0L, 20L);
+    }
+
+    public void updatePets() {
+
+        for (Map.Entry<UUID, Pet> entry : playerPets.entrySet()) {
+
+            Pet pet = entry.getValue();
+
+            Player player = plugin.getServer().getPlayer(entry.getKey());
+
+            if (pet.getLocation() == null) continue;
+
+            if (pet.getLocation().distanceSquared(player.getLocation()) <= 10) continue;
+
+            pet.getEntity().teleport(player.getLocation());
+        }
+    }
 
     public void renamePet(Player player, String customName) {
         Pet pet = getPet(player);
